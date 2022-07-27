e8dff5692fba55829c72531b2fc4bb6df0182135
Sainan <63328889+Sainan@users.noreply.github.com>
2022-07-27 08:39:41 +0200

    Recompile lua with LUA_USE_LONGJMP

--- build scripts/build_lua.php
+++ build scripts/build_lua.php
@@ -1,5 +1,5 @@
 <?php
-$clang = "em++ -O3 -flto -x c++ -std=c++20 -fvisibility=hidden";
+$clang = "em++ -O3 -flto -x c++ -std=c++20 -fvisibility=hidden -D LUA_USE_LONGJMP";
 
 // Setup folders
 if(!is_dir("bin"))
